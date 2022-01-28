87eb053cbc6de72bc250fa76f8df20c5fdda1515
Bedheadhere <83078551+Bedheadhere@users.noreply.github.com>
2022-01-28 21:39:43 +0000

    BasicPlayerBranch

---
+++ Trash Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour { 
+
+    [Header("Components")]
+    Rigidbody m_Rigidbody;
+
+    [Header("MovementVariables")]
+    Vector3 moveDirection;
+    public float movementSpeed;
+    public float jumpForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rigidbody.velocity = moveDirection.normalized * movementSpeed;
+    }
+
+    private void MovementInput()
+    {
+        // Horizontal and Vertical movement
+        float xAxisInput = Input.GetAxisRaw("Horizontal");
+        float zAxisInput = Input.GetAxisRaw("Vertical");
+
+        // Jump check
+
+
+        moveDirection = new Vector3(xAxisInput, 0.0f, zAxisInput);
+    }
+}
