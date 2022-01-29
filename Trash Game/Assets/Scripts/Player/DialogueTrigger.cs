3558dc622d6ea82d9697e0081f4ede57dab75814
RenDeveloper <duncansrendall@gmail.com>
2022-01-29 12:24:00 +0000

    finished dialogue system

---
+++ Trash Game/Assets/Scripts/Player/DialogueTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public BoxCollider npcTriggerbox;
+    public bool inRange = false;
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.E) && inRange)
+        {
+            TriggerDialogue();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Debug.Log("plyaer");
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("player exit");
+            inRange = false;
+        }
+    }
+}
