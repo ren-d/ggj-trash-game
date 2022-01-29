fc660154ba664fa59c1644675714106d7a77c120
Bedheadhere <83078551+Bedheadhere@users.noreply.github.com>
2022-01-29 19:58:47 +0000

    pause menu

---
+++ Trash Game/Assets/Scripts/Menu/PauseMenuButtons.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuButtons : MonoBehaviour
+{
+    GameObject pauseMenu;
+    bool isPaused = false;
+    bool isMainMenu = true;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        pauseMenu = GetComponent<Transform>().GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        if (!isMainMenu)
+        {
+            if (Input.GetButtonDown("Cancel") && !isPaused)
+            {
+                Pause();
+            }
+            else if (Input.GetButtonDown("Cancel") && isPaused)
+            {
+                Unpause();
+            }
+        }  
+    }
+
+    public void OnStartPlay()
+    {
+        isMainMenu = false;
+    }
+
+    public void Pause()
+    {
+        FindObjectOfType<GameStateManager>().currentState = GameStateManager.GameState.PAUSED;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+    public void Unpause()
+    {
+        FindObjectOfType<GameStateManager>().currentState = GameStateManager.GameState.PLAYING;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene(0);
+        GameStateManager gameStateManager = FindObjectOfType<GameStateManager>();
+        Destroy(gameStateManager);
+        Destroy(gameObject);
+    }
+
+    public void Continue()
+    {
+        FindObjectOfType<GameStateManager>().currentState = GameStateManager.GameState.PLAYING;
+        isPaused = false;
+    }
+}
