f12727c1062604124b4becef2b479d7c13867c00
RenDeveloper <duncansrendall@gmail.com>
2022-01-30 14:39:52 +0000

    base input

--- Trash Game/Assets/Scripts/animHandler.cs
+++ Trash Game/Assets/Scripts/animHandler.cs
@@ -6,16 +6,36 @@ public class animHandler : MonoBehaviour
 {
     private Animator animator;
 
-    private void Start()
+
+    void Start()
     {
         animator = GetComponent<Animator>();
+        animator.SetBool("isMoving", false);
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        while (true)
+        {
+            animator.SetBool("play", true);
+            yield return new WaitForSeconds(1);
+            animator.SetBool("play", false);
+            yield return new WaitForSeconds(2);
+        }
     }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.E))
+        
+        if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
         {
-            animator.SetFloat("Blend", 1);
+            animator.SetBool("isMoving", false);
         }
+        else
+        {
+            animator.SetBool("isMoving", true);
+        }
+
     }
 }
