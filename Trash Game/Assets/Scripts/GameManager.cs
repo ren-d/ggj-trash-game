3246001f08a45cf8b495573811da75c85819e324
RenDeveloper <duncansrendall@gmail.com>
2022-03-02 20:55:32 +0000

    Added gamestates

--- Trash Game/Assets/Scripts/GameManager.cs
+++ Trash Game/Assets/Scripts/GameManager.cs
@@ -2,22 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    PAUSED = 0,
+    PLAYING,
+    MENU,
+    DIALOG
+}
+
+public enum WorldState
+{
+    OUTSIDE = 0,
+    INSIDE,
+    MENU
+}
+//spafce
 public class GameManager : MonoBehaviour
 {
-    enum GameState
-    { 
-        PAUSED = 0,
-        PLAYING,
-        MENU,
 
-    
-    }
+
 
     private static GameManager _instance;
 
     public static GameManager Instance
     { 
-        //This instance of game manager can't be set to anything other than _instance
+        // This instance of game manager can't be set to anything other than _instance
         get { 
 
             return _instance; 
@@ -26,7 +35,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        //if another instance exists on the loading of a new scene it destroys the instance
+        // if another instance exists on the loading of a new scene it destroys the instance
         if(_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
@@ -36,6 +45,16 @@ public class GameManager : MonoBehaviour
             _instance = this;
         }
     }
+ 
+    // Game State
+    private GameState _currentState = GameState.MENU;
 
-    
+    public void setGameState(GameState state) { _currentState = state; }
+    public GameState getGameState() { return _currentState; }
+
+    // World State
+    private WorldState _currentWorld = WorldState.MENU;
+
+    public void setWorldState(WorldState state) { _currentWorld = state; }
+    public WorldState getWorldState() { return _currentWorld; }
 }
